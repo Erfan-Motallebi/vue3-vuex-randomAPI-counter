e6fb7d524b59726ff84ad7afc885099c87689c0d
Erfan <erfan.fulldevs@gmail.com>
2021-04-20 11:08:13 +0430

    added store.js to make it

---
+++ src/store/Store.js
@@ -0,0 +1,33 @@
+import { createStore } from "vuex";
+import axios from 'axios';
+
+const Store = createStore({
+    state: {
+        counter: 0,
+    },
+    mutations: {
+        increaseCounter(state, randomAPI) {
+            state.counter += randomAPI
+        },
+        decreaseCounter(state, randomAPI) {
+            state.counter -= randomAPI
+        }
+    },
+    actions: {
+        increaseCounter({ commit }) {
+            axios('https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new').then(response => {
+                commit('increaseCounter', response.data)
+            })
+        },
+        decreaseCounter({ commit }) {
+            axios('https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new').then(res => {
+                commit('decreaseCounter', res.data)
+            })
+        }
+    },
+    getters: {},
+    modules: {}
+
+})
+
+export default Store;
